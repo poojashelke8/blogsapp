6220dd7b7ad35b5eaee97a24ff99b976f393d67c
poojashelke8 <poojabshelke0807@gmail.com>
2023-12-30 13:28:42 +0530

    added postlist component

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,11 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+// import { sub } from 'date-fns';
 const initialState =[
     {
         id: '1',
         title: 'Learning Redux Toolkit',
         content: "I've heard good things.",
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        // date: sub(new Date(), { minutes: 10 }).toISOString(),
         reactions: {
             thumbsUp: 0,
             wow: 0,
@@ -18,7 +18,7 @@ const initialState =[
         id: '2',
         title: 'Slices...',
         content: "The more I say slice, the more I want pizza.",
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        // date: sub(new Date(), { minutes: 5 }).toISOString(),
         reactions: {
             thumbsUp: 0,
             wow: 0,
@@ -36,5 +36,7 @@ const postSlice = createSlice({
 
     }
 })
+
+export const selectAllPost = (state)=>state.posts;
 
 export default postSlice.reducer;
